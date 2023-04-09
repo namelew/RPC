a660cf7d992f6ab684a7984c364fb17a1869c79d
namelew <diogomaciel.cunha@gmail.com>
2023-04-09 15:38:04 -0300

    adding command user interface

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,9 +1,69 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
 	"github.com/namelew/RPC/internal/rpc"
+	"github.com/namelew/RPC/packages/messages"
 )
+
+func sanitaze(s string) string {
+	trash := []string{"\n", "\b", "\r", "\t"}
+
+	for i := range trash {
+		s = strings.ReplaceAll(s, trash[i], "")
+	}
+
+	return s
+}
 
 func main() {
 	go rpc.Listen()
+
+	r := bufio.NewReader(os.Stdin)
+
+	for {
+		fmt.Print("\nProcedure: ")
+		p, err := r.ReadSlice('\n')
+
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+
+		input := strings.Split(string(p), " ")
+
+		if len(input) < 3 {
+			continue
+		}
+
+		var m messages.Message
+
+		a, err := strconv.Atoi(input[1])
+
+		if err != nil {
+			log.Println(err.Error())
+			continue
+		}
+
+		m.Action = messages.Action(a)
+
+		for _, num := range strings.Split(input[2], ",") {
+			a, err := strconv.Atoi(sanitaze(num))
+
+			if err != nil {
+				log.Println(err.Error())
+				continue
+			}
+
+			m.Payload = append(m.Payload, int64(a))
+		}
+
+		rpc.ResquestProcess(input[0], m)
+	}
 }
